7a792588bc7c19633085a94df2b45279149e0579
Michael Witt <mjwitt@mtu.edu>
2011-02-01 09:45:37 -0500

    Add ServerNetConnectionManager to itracker.backend.server.net
    Signed-off-by: Michael Witt <mjwitt@mtu.edu>

---
+++ src/itracker/backend/server/net/ServerNetConnectionManager.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package itracker.backend.server.net;
+
+/**
+ * Network connection manager class for the server
+ * 
+ * 
+ * @author Michael Witt
+ *
+ */
+public class ServerNetConnectionManager {
+
+}
