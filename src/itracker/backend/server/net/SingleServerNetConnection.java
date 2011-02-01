01206f7613aeba607dbe2206dc23ed3cba54fd9e
Michael Witt <mjwitt@mtu.edu>
2011-02-01 09:42:25 -0500

    Initial Add SingleServerNetConnection in package itracker.backend.server.net
    Signed-off-by: Michael Witt <mjwitt@mtu.edu>

---
+++ src/itracker/backend/server/net/SingleServerNetConnection.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package itracker.backend.server.net;
+
+/**
+ * Wrapper for a single server net connection
+ * 
+ * Should be implemented as part of a larger server 
+ * to accept multiple connections
+ * 
+ * Can/should be used in a threaded environment
+ * as it contains blocking methods, such as accept
+ * 
+ * 
+ * @author Michael Witt
+ *
+ */
+public class SingleServerNetConnection {
+
+}
