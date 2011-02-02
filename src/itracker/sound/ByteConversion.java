042663c30ed4439aa4556a4eef14447ee61c6ebe
Michael Witt <mjwitt@mtu.edu>
2011-02-02 14:38:17 -0500

    Adding 2 classes with static functions. These will be instrumental in later development, as they will be the basis for all audio generation.
    SoundFunctions will contain methods that generate and apply filters to
    sinusoids
    
    ByteConversion will convert integers and integer arrays into byte
    arrays using the specified encoding. Encoding will be specified by
    selecting the proper inner class.
    
    Signed-off-by: Michael Witt <mjwitt@mtu.edu>

---
+++ src/itracker/sound/ByteConversion.java
@@ -0,0 +1,202 @@
+/**
+ * 
+ */
+package itracker.sound;
+
+/**
+ * All methods in this class are static, so that they can be
+ * called without instantiating this object.
+ * 
+ * This class contains functions that will convert single
+ * numbers to byte arrays, in big or little endian encoding
+ * 
+ * TODO: JAVADOCS
+ * TODO: CODE
+ * 
+ * @author Michael Witt
+ *
+ */
+public class ByteConversion {
+	
+	/**
+	 * Static inner class containing methods that return byte arrays
+	 * in the Big Endian encoding system.
+	 * 
+	 * Methods should be referencable without instantiation.
+	 * 
+	 * @author Michael Witt
+	 *
+	 */
+	public static class BigEndian {
+		/**
+		 * Convert a signed integer to an array of bytes.
+		 * Bit depth is assumed to be the smallest multiple of 8
+		 * that can store the number passed to the method
+		 * @param toConvert	number to be converted
+		 * @return byte array containing the bytes of the number
+		 */
+		public static byte [] signedToByteArray(int toConvert) {
+			//TODO: Write Method to convert signed ints to byte arrays
+			return null;
+		}
+		
+		/**
+		 * Convert a signed integer array to an array of bytes.
+		 * Bit depth is assumed to be the smallest multiple of 8
+		 * that can store the numbers passed to the method
+		 * @param toConvert array to be converted
+		 * @return byte array containing the contents of the old array
+		 */
+		public static byte [] signedToByteArray(int [] toConvert) {
+			//TODO: Write Method to convert signed int arrays to byte arrays
+			return null;
+		}
+		
+		/**
+		 * TODO: JAVADOC EVERYTHING
+		 * @param toConvert 
+		 * @return
+		 */
+		public static byte [] unsignedToByteArray(int toConvert) {
+			//TODO: Write Method to convert unsigned ints to byte arrays
+			return null;
+		}
+		
+		public static byte [] unsignedToByteArray(int [] toConvert) {
+			//TODO: Write Method to convert unsigned int arrays to byte arrays
+			return null;
+		}
+		
+		public static byte [] signedToByteArray(int toConvert, int depth) {
+			//TODO: Write Method to convert signed ints to byte arrays
+			return null;
+		}
+		
+		public static byte [] signedToByteArray(int [] toConvert, int depth) {
+			//TODO: Write Method to convert signed int arrays to byte arrays
+			return null;
+		}
+		
+		public static byte [] unsignedToByteArray(int toConvert, int depth) {
+			//TODO: Write Method to convert unsigned ints to byte arrays
+			return null;
+		}
+		
+		public static byte [] unsignedToByteArray(int [] toConvert, int depth) {
+			//TODO: Write Method to convert unsigned int arrays to byte arrays
+			return null;
+		}
+		
+		public static byte [][] signedToByteArrayBuffered(int toConvert, int depth, int maxSize) {
+			//TODO: Write Method to convert signed ints to byte arrays
+			return null;
+		}
+		
+		public static byte [][] signedToByteArrayBuffered(int [] toConvert, int depth, int maxSize) {
+			//TODO: Write Method to convert signed int arrays to byte arrays
+			return null;
+		}
+		
+		public static byte [][] unsignedToByteArrayBuffered(int toConvert, int depth, int maxSize) {
+			//TODO: Write Method to convert unsigned ints to byte arrays
+			return null;
+		}
+		
+		public static byte [][] unsignedToByteArrayBuffered(int [] toConvert, int depth, int maxSize) {
+			//TODO: Write Method to convert unsigned int arrays to byte arrays
+			return null;
+		}
+	}
+	
+	/**
+	 * Static inner class containing methods that return byte arrays
+	 * in the Little Endian encoding system.
+	 * 
+	 * Methods should be referencable without instantiation.
+	 * 
+	 * @author Michael Witt
+	 *
+	 */
+	public static class LittleEndian {
+		/**
+		 * Convert a signed integer to an array of bytes.
+		 * Bit depth is assumed to be the smallest multiple of 8
+		 * that can store the number passed to the method
+		 * @param toConvert	number to be converted
+		 * @return byte array containing the bytes of the number
+		 */
+		public static byte [] signedToByteArray(int toConvert) {
+			//TODO: Write Method to convert signed ints to byte arrays
+			return null;
+		}
+		
+		/**
+		 * Convert a signed integer array to an array of bytes.
+		 * Bit depth is assumed to be the smallest multiple of 8
+		 * that can store the numbers passed to the method
+		 * @param toConvert array to be converted
+		 * @return byte array containing the contents of the old array
+		 */
+		public static byte [] signedToByteArray(int [] toConvert) {
+			//TODO: Write Method to convert signed int arrays to byte arrays
+			return null;
+		}
+		
+		/**
+		 * TODO: JAVADOC EVERYTHING
+		 * @param toConvert 
+		 * @return
+		 */
+		public static byte [] unsignedToByteArray(int toConvert) {
+			//TODO: Write Method to convert unsigned ints to byte arrays
+			return null;
+		}
+		
+		public static byte [] unsignedToByteArray(int [] toConvert) {
+			//TODO: Write Method to convert unsigned int arrays to byte arrays
+			return null;
+		}
+		
+		public static byte [] signedToByteArray(int toConvert, int depth) {
+			//TODO: Write Method to convert signed ints to byte arrays
+			return null;
+		}
+		
+		public static byte [] signedToByteArray(int [] toConvert, int depth) {
+			//TODO: Write Method to convert signed int arrays to byte arrays
+			return null;
+		}
+		
+		public static byte [] unsignedToByteArray(int toConvert, int depth) {
+			//TODO: Write Method to convert unsigned ints to byte arrays
+			return null;
+		}
+		
+		public static byte [] unsignedToByteArray(int [] toConvert, int depth) {
+			//TODO: Write Method to convert unsigned int arrays to byte arrays
+			return null;
+		}
+		
+		public static byte [][] signedToByteArrayBuffered(int toConvert, int depth, int maxSize) {
+			//TODO: Write Method to convert signed ints to byte arrays
+			return null;
+		}
+		
+		public static byte [][] signedToByteArrayBuffered(int [] toConvert, int depth, int maxSize) {
+			//TODO: Write Method to convert signed int arrays to byte arrays
+			return null;
+		}
+		
+		public static byte [][] unsignedToByteArrayBuffered(int toConvert, int depth, int maxSize) {
+			//TODO: Write Method to convert unsigned ints to byte arrays
+			return null;
+		}
+		
+		public static byte [][] unsignedToByteArrayBuffered(int [] toConvert, int depth, int maxSize) {
+			//TODO: Write Method to convert unsigned int arrays to byte arrays
+			return null;
+		}
+	}
+	
+
+}
---
+++ src/itracker/sound/SoundFunctions.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package itracker.sound;
+
+/**
+ * TODO: Update this javadoc
+ * Common functions to create sound elements, such as convolution, adsr,
+ * sinusoid generation, etc...
+ * 
+ * Note that the output from this function CANNOT be passed to the sound
+ * driver. Most of these functions will return arrays of integers, which
+ * then need to be converted to bytes, with a specific byte order. Those
+ * then need to be broken up into chunks small enough to fit into a
+ * buffer that will output the sound.
+ * 
+ * @author Michael Witt
+ *
+ */
+public class SoundFunctions {
+	
+	//TODO: Sinusoid generation
+	//TODO: Sinusoid addition
+	//TODO: Sinusoid addition with compression
+	//TODO: Sinusoid addition with compression and normalization
+	//TODO: Discrete Fourier Transforms
+	//TODO: Convolution
+	//TODO: ADSR Envelope
+	//TODO: FIR Filter function
+	//TODO: IIR Filter function (?)
+	//TODO: Generic envelope function
+	
+}
